7d8b379875583e396b38a340ba176fc310f678ab
yunusemretokyay1 <101728985+yunusemretokyay1@users.noreply.github.com>
2024-02-05 14:08:53 +0100

    Basic annotations - concat

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatenates two strings and returns the result.
+
+    Parameters:
+    - str1 (str): The first string.
+    - str2 (str): The second string.
+
+    Returns:
+    str: The concatenation of the two input strings.
+    """
+    return str1 + str2
