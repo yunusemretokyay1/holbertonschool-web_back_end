8c29ae825fe08ac28d419f7604fefb7fbe6b6718
yunusemretokyay1 <101728985+yunusemretokyay1@users.noreply.github.com>
2024-02-05 16:01:41 +0100

    Basic annotations -to string

--- python_variable_annotations/3-to_str.py
+++ python_variable_annotations/3-to_str.py
@@ -7,9 +7,9 @@ Uses Python type hints to specify that
 
 
 def to_str(n: float) -> str:
-     """
+    """
     Assuming that 'n' is a float,
     returns the string representation
     of 'n'.
     """
-     return str(n)
+    return str(n)
