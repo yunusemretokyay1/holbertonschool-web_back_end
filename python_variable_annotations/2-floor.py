c05e7410ee5d10da1b028388fb681c8d0fa77006
yunusemretokyay1 <101728985+yunusemretokyay1@users.noreply.github.com>
2024-02-05 14:27:36 +0100

    Basic annotations - floor

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+def floor(n: float) -> int:
+    """
+    Returns the floor (rounded down) of the input floating-point number 'n'.
+
+    Parameters:
+    - n (float): The input floating-point number.
+
+    Returns:
+    int: The result of rounding down 'n' to the nearest integer.
+    """
+    return int(n)
