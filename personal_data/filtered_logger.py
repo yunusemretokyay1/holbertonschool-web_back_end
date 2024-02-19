3e6cecabb1544e76f2047eeaf1b91081c39293e2
yunusemretokyay1 <101728985+yunusemretokyay1@users.noreply.github.com>
2024-02-19 18:22:25 +0100

    Regex-ing

---
+++ personal_data/filtered_logger.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+Learning about keeping the user's data safe.
+
+This file is meant to be run in a server.
+"""
+from typing import List
+import re
+
+
+def filter_datum(
+        fields: List[str],
+        redaction: str,
+        message: str,
+        separator: str) -> str:
+    """
+    'message' should be similar to a CSV format:
+    <field name>=<field value>{separator}<field name>=<field value>{separator}
+    (I believe it should end with a separator)
+
+    Returns 'message', but each field that has a name that's in 'fields'
+    has its value replaces with 'redacion'.
+    """
+    result = message
+    for field_name in fields:
+        PATTERN = f"(?<={field_name}=)(.*?)(?={separator})"
+        result = re.sub(PATTERN, redaction, result)
+    return result
