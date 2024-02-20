0bb178e4896e59e764ba5b07e45b982034a27769
yunusemretokyay1 <101728985+yunusemretokyay1@users.noreply.github.com>
2024-02-20 15:24:59 +0100

    Encrypting passwords

---
+++ personal_data/encrypt_password.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Exercise 5: Make a function that takes in a password string,
+and returns the password salted and hashed using 'bcrypt.hashpw'.
+
+The return type should be a byte string.
+
+Exercise 6: Make a function that takes in the hashed and salted
+version of a password, and a new string called 'password',
+and checks if this new 'password' is valid, using 'bcrypt'.
+"""
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """
+    Returns the password salted and hashed,
+    with 'bcrypt.hashpw(password.encode(), bcrypy.gensalt())'.
+    """
+    return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
