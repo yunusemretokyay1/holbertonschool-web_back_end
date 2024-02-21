cbdbe5d218663a39ef35478b82507b34eafe5574
yunusemretokyay1 <101728985+yunusemretokyay1@users.noreply.github.com>
2024-02-21 01:54:14 +0100

    Error handler: Unauthorized

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 from os import getenv
 from api.v1.views import app_views
-from flask import Flask, jsonify, abort, request
+#from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
 
