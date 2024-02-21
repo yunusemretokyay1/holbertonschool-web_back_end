f91f60988e9b0be45e0b20b48c329dd6ee7c296e
yunusemretokyay1 <101728985+yunusemretokyay1@users.noreply.github.com>
2024-02-21 16:50:51 +0100

    Error handler: Forbidden

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -7,19 +7,12 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
-from api.v1.auth.auth import Auth
-from api.v1.auth.basic_auth import BasicAuth
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
-
-if os.getenv("AUTH_TYPE") == 'basic_auth':
-    auth = BasicAuth()
-else:
-    auth = Auth()
 
 
 @app.errorhandler(404)
