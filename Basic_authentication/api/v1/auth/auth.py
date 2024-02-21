3bce7bd06f259e26423ce935e8675a9f375c5765
yunusemretokyay1 <101728985+yunusemretokyay1@users.noreply.github.com>
2024-02-21 17:09:24 +0100

    Auth class

---
+++ Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+""" Auth module
+"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth():
+    """
+    Auth class
+    """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        require_auth function
+        """
+        
+        def authorization_header(self, request=None) -> str:
+        """
+        authorization_header function
+        """
+    
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        current_user function
+        """
+        return None
