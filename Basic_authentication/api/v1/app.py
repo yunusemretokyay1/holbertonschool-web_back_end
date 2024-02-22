cf749381ef18b354c0068014a2395b28b2fdbcce
yunusemretokyay1 <101728985+yunusemretokyay1@users.noreply.github.com>
2024-02-22 16:32:07 +0100

    . Basic - Overload current_user - and BOOM!

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-""" Auth module
+"""
+Route module for the API
 """
 from os import getenv
 from api.v1.views import app_views
@@ -10,38 +11,58 @@ from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
 
 
-class BasicAuth(Auth):
+app = Flask(__name__)
+app.register_blueprint(app_views)
+CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
+
+if os.getenv("AUTH_TYPE") == 'basic_auth':
+    auth = BasicAuth()
+else:
+    auth = Auth()
+
+
+@app.errorhandler(404)
+def not_found(error) -> str:
+    """ Not found handler
     """
-    Basic Auth class
+    return jsonify({"error": "Not found"}), 404
+
+
+@app.errorhandler(401)
+def not_authorized(error) -> str:
+    """ Not Authorized handler
+    """
+    return jsonify({"error": "Unauthorized"}), 401
+
+
+@app.errorhandler(403)
+def forbidden(error) -> str:
+    """ Forbidden handler
+    """
+    return jsonify({"error": "Forbidden"}), 403
+
+
+@app.before_request
+def before_request():
+    """ Before_request handler
     """
 
-    def extract_base64_authorization_header(self,
-                                            authorization_header: str) -> str:
-        """
-        extract_base64_authorization_header function
-        """
-        if authorization_header is None:
-            return None
-        if type(authorization_header) is not str:
-            return None
-        if authorization_header[0:6] != 'Basic ':
-            return None
-        return authorization_header[6:]
+    if auth is None:
+        return
 
-    def decode_base64_authorization_header(self,
-                                           base64_authorization_header: str
-                                           ) -> str:
-        """
-        decode_base64_authorization_header function
-        """
-        if base64_authorization_header is None:
-            return None
-        if type(base64_authorization_header) is not str:
-            return None
-        try:
-            base64_bytes = base64_authorization_header.encode("utf-8")
-            sample_string_bytes = base64.b64decode(base64_bytes)
-            sample_string = sample_string_bytes.decode("utf-8")
-            return sample_string
-        except Exception:
-            return None
+    _paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
+    if not auth.require_auth(request.path, _paths):
+        return
+
+    if auth.authorization_header(request) is None:
+        abort(401)
+
+    if auth.current_user(request) is None:
+        abort(403)
+
+
+if __name__ == "__main__":
+    host = getenv("API_HOST", "0.0.0.0")
+    port = getenv("API_PORT", "5000")
+    app.run(host=host, port=port)
