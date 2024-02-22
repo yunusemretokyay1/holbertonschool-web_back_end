df8b4f095513523cfff8d00531af14cb5cc34522
yunusemretokyay1 <101728985+yunusemretokyay1@users.noreply.github.com>
2024-02-22 11:52:11 +0100

    Basic - Base64 part

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,10 +4,22 @@
 from api.v1.auth.auth import Auth
 import base64
 from typing import TypeVar
-from models.user import User
 
 
 class BasicAuth(Auth):
     """
     Basic Auth class
     """
+
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """
+        extract_base64_authorization_header function
+        """
+        if authorization_header is None:
+            return None
+        if type(authorization_header) is not str:
+            return None
+        if authorization_header[0:6] != 'Basic ':
+            return None
+        return authorization_header[6:]
