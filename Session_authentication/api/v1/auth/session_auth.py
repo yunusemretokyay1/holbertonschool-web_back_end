a85825d3c7492a7f848255e5bf12cbd37debab7b
yunusemretokyay1 <101728985+yunusemretokyay1@users.noreply.github.com>
2024-02-23 00:17:11 +0100

    Empty session

---
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+""" Session auth module
+"""
+
+from api.v1.auth.auth import Auth
+from uuid import uuid4
+from models.user import User
+
+
+class SessionAuth(Auth):
+    """ Extend behavior of Auth class for session authentication. """
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """ Create and return a session ID for a user ID. """
+        if user_id is None or not isinstance(user_id, str):
+            return None
+
+        session_id = str(uuid4())
+        SessionAuth.user_id_by_session_id[session_id] = user_id
+
+        return session_id
