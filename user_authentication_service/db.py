a6bc3cdc6ffbf487112e91ecfd8e6eef1fc55a32
yunusemretokyay1 <yunusemretokyay@gmail.com>
2024-02-26 16:04:14 +0100

    Find user

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -37,3 +37,22 @@ class DB:
         self._session.commit()
 
         return user
+
+    def find_user_by(self, **kwargs) -> User:
+        """ Finds user by key word args
+        Return: First row found in the users table as filtered by kwargs
+        """
+        if not kwargs:
+            raise InvalidRequestError
+
+        column_names = User.__table__.columns.keys()
+        for key in kwargs.keys():
+            if key not in column_names:
+                raise InvalidRequestError
+
+        user = self._session.query(User).filter_by(**kwargs).first()
+
+        if user is None:
+            raise NoResultFound
+
+        return user
