30aeded97eb1b46d7b3fe410c6955a929b3b1d79
yunusemretokyay1 <yunusemretokyay@gmail.com>
2024-02-27 15:14:01 +0100

    Register user

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -17,16 +17,17 @@ def hello_world() -> str:
     msg = {"message": "Bienvenue"}
     return jsonify(msg)
 
+
 @app.route('/users', methods=['POST'])
 def register_user() -> str:
     """ Register a new user """
     try:
        email = request.form['email']
-        password = request.form['password']
+       password = request.form['password']
     except KeyError:
         abort(400)
 
-     try:
+    try:
         user = AUTH.register_user(email, password)
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
