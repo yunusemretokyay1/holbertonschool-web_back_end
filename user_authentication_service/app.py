9ef8500606b980ad0ffac48d2eb2801ea391a433
yunusemretokyay1 <yunusemretokyay@gmail.com>
2024-02-27 14:54:42 +0100

    basic flask app

---
+++ user_authentication_service/app.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""API Routes for Authentication Service"""
+from auth import Auth
+from flask import (Flask,
+                   jsonify,
+                   request,
+                   abort,
+                   redirect)
+
+app = Flask(__name__)
+AUTH = Auth()
+
+
+@app.route('/', methods=['GET'])
+def hello_world() -> str:
+    """ Base route for authentication service API """
+    msg = {"message": "Bienvenue"}
+    return jsonify(msg)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
+
