f81fe8481efa67dee9202cee99170bfc4f7f7065
yunusemretokyay1 <yunusemretokyay@gmail.com>
2024-02-27 11:51:13 +0100

    Hash password

---
+++ user_authentication_service/auth.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" Authentication Module """
+
+import bcrypt
+from db import DB
+from sqlalchemy.orm.exc import NoResultFound
+from typing import Union
+from user import User
+
+
+
+def _hash_password(password: str) -> str:
+    """ Returns a salted hash of the input password """
+    hashed = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+    return hashed
+
