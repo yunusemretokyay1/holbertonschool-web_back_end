b426f0fe0e706d6f817fccc0956ba539f263d8a4
yunusemretokyay1 <yunusemretokyay@gmail.com>
2024-02-28 17:47:16 +0100

    Log out

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -58,5 +58,26 @@ def log_in() -> str:
     return response
 
 
+@app.route('/sessions', methods=['DELETE'])
+def log_out() -> str:
+    """Find the user with the requested session ID.
+    If the user exists destroy the session and redirect the user to GET /.
+    If the user does not exist, respond with a 403 HTTP status.
+    """
+    session_id = request.cookies.get("session_id", None)
+
+    if session_id is None:
+        abort(403)
+
+    user = AUTH.get_user_from_session_id(session_id)
+
+    if user is None:
+        abort(403)
+
+    AUTH.destroy_session(user.id)
+
+    return redirect('/')
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
