a5b661a11507c2e526314d034a4c1d1fb5d5b86a
yunusemretokyay1 <yunusemretokyay@gmail.com>
2024-02-28 17:13:52 +0100

    Find user by session ID

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -71,3 +71,17 @@ class Auth:
         self._db.update_user(user.id, session_id=session_id)
 
         return session_id
+
+    def get_user_from_session_id(self, session_id: str) -> Union[str, None]:
+        """It takes a single session_id string argument
+        Returns a string or None
+        """
+        if session_id is None:
+            return None
+
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+        except NoResultFound:
+            return None
+
+        return user
