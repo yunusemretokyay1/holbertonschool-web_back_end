97d4381ff78f89d3f0a83c8e3957f45a03fbe127
yunusemretokyay1 <yunusemretokyay@gmail.com>
2024-02-29 11:54:35 +0100

    Generate reset password token

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -97,7 +97,7 @@ class Auth:
 
         return None
 
-     def get_reset_password_token(self, email: str) -> str:
+    def get_reset_password_token(self, email: str) -> str:
         """Generates a reset password token if user exists"""
         try:
             user = self._db.find_user_by(email=email)
@@ -109,4 +109,3 @@ class Auth:
         self._db.update_user(user.id, reset_token=reset_token)
 
         return reset_token
-
