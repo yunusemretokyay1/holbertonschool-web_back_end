964fbe2a7ee3cced5760ebc1899565b8d6d437a3
yunusemretokyay1 <yunusemretokyay@gmail.com>
2024-03-06 02:24:46 +0100

    Integration test: fixtures

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -2,8 +2,8 @@
 """ Module for testing client """
 
 from client import GithubOrgClient
-from parameterized import parameterized, parameterized_class
 from fixtures import TEST_PAYLOAD
+from parameterized import parameterized, parameterized_class
 import json
 import unittest
 from unittest.mock import patch, PropertyMock, Mock
