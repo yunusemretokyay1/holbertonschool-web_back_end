1cc4e21fb76dcbaa792ee19c6d0e6fb5e34d73f8
yunusemretokyay1 <101728985+yunusemretokyay1@users.noreply.github.com>
2024-03-07 14:00:52 +0100

    locale

--- i18n/2-app.py
+++ i18n/2-app.py
@@ -2,6 +2,8 @@
 """ Module  Babel i18n """
 from flask_babel import Babel
 from flask import Flask, render_template, request
+
+
 app = Flask(__name__, template_folder='templates')
 babel = Babel(app)
 
