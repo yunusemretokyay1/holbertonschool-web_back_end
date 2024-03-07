f16c319f9bc9134bb3186bcdbb41ebdd06ccaf6f
yunusemretokyay1 <yunusemretokyay@gmail.com>
2024-03-07 12:53:32 +0100

    Get locale from request

--- i18n/2-app.py
+++ i18n/2-app.py
@@ -22,7 +22,13 @@ app.config.from_object(Config)
 @app.route('/', methods=['GET'], strict_slashes=False)
 def hello_world() -> str:
     """Renders a Basic Template """
-    return render_template("1-index.html")
+    return render_template("2-index.html")
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """Select a language """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 if __name__ == "__main__":
