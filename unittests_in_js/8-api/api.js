55e31b413dc7add678a8afb5607658cd20cff979
yunusemretokyay1 <yunusemretokyay@gmail.com>
2024-03-26 16:26:01 +0100

    Basic Integration testing

---
+++ unittests_in_js/8-api/api.js
@@ -0,0 +1,8 @@
+const express = require('express');
+
+const port = 7865;
+const app = express();
+app.get('/', (request, response) => {
+  response.send('Welcome to the payment system');
+});
+app.listen(port, () => console.log(`API available on localhost port ${port}`));
