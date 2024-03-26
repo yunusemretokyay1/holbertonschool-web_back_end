3d7de52a7337722a0686fa13b823184e56f71bf6
yunusemretokyay1 <yunusemretokyay@gmail.com>
2024-03-26 15:16:21 +0100

    Async tests with done

---
+++ unittests_in_js/6-payment_token.test.js
@@ -0,0 +1,17 @@
+const getPaymentTokenFromAPI = require('./6-payment_token');
+const chai = require('chai');
+
+describe('getPaymentTokenFromAPI', () => {
+  it('returns a resolved promise when called with true', (done) => {
+    getPaymentTokenFromAPI(true)
+      .then((data) => {
+        try{
+          chai.expect(data).to.eql({ data: 'Successful response from the API' });
+          done();
+        } catch (error) {
+          done(error);
+        }
+      });
+  });
+
+});
